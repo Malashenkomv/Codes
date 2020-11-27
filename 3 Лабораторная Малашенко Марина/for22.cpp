374b9ea835cad6950cd0fde327cad2c61f3997f1
Malashenkomv <74235910+Malashenkomv@users.noreply.github.com>
2020-11-27 23:12:08 +0300

    .

---
+++ 3 Лабораторная Малашенко Марина/for22.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float X, f = 1, sum = 1;
+    int N, i, j;
+    cin >> X >> N;
+    
+    int M = N;
+    for (i = 1; i <= N; i++) {
+    for (j = 1; j <= M; j++) {
+        f *= j; 
+    } 
+    sum += pow (X, N) / f;
+    cout << pow (X, N) / f << endl;
+    f = 1;
+    M--;
+    }
+    
+    cout << "Приближенное значение экспоненты в точке X с точностью N равно: " ;
+    cout << sum ;
+
+    return 0;
+}
